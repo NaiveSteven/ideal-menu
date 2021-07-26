346ed2fb18291330ec00e148ad505972a8b371cd
qinmingjie <1004537433@qq.com>
2021-07-27 00:27:50 +0800

    feat: menu init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
-  <div id="app">
-    <Menu/>
+  <div name="app">
+    <Menu :data="data" />
   </div>
 </template>
 
 <script>
-import Menu from './components/Menu.vue'
+import Menu from "./components/Menu.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Menu
-  }
+    Menu,
+  },
+  data() {
+    return {
+      data: [
+        { title: "根 1", name: 1 },
+        {
+          title: "根 2",
+          name: 2,
+          children: [
+            {
+              title: "根 2-1",
+              name: 21,
+              children: [
+                { title: "根 2-1-1", name: 211 },
+                { title: "根 2-1-2", name: 212 },
+              ],
+            },
+          ],
+        },
+        { title: "根 3", name: 3 },
+        {
+          title: "根 4",
+          name: 4,
+          children: [
+            { title: "根 4-1", name: 41 },
+            { title: "根 4-2", name: 42 },
+          ],
+        },
+      ],
+    }
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url("./style/index.scss");
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,16 +1,46 @@
-<template>
-  <div>asdfsdf</div>
-</template>
-
 <script>
+import MenuItem from "./MenuItem.vue"
+import SubMenu from "./SubMenu.vue"
 export default {
-  name: 'Menu',
+  name: "Menu",
+  components: { MenuItem, SubMenu },
   props: {
-    msg: String
-  }
+    data: {
+      type: Array,
+      default: () => [],
+    },
+    activeName: {
+      type: String,
+      default: ''
+    },
+  },
+  methods: {
+    childrenMenuRender(menuItemData) {
+      return (
+        <li>
+          {menuItemData.children && (
+            <SubMenu key={menuItemData.name}>
+              <div slot="title">{menuItemData.title}</div>
+              {menuItemData.children.map((item) => {
+                return this.childrenMenuRender(item)
+              })}
+            </SubMenu>
+          )}
+          {!menuItemData.children && (
+            <MenuItem key={menuItemData.name}>{menuItemData.title}</MenuItem>
+          )}
+        </li>
+      )
+    },
+  },
+  render() {
+    return (
+      <ul>
+        {this.data.map((item) => {
+          return this.childrenMenuRender(item)
+        })}
+      </ul>
+    )
+  },
 }
 </script>
-
-<style scoped>
-
-</style>
