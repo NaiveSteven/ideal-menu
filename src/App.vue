b8f9c6585f78fd5f4166ca7bd6aa74649c62802c
qinmingjie <1004537433@qq.com>
2021-07-26 23:24:45 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Menu/>
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Menu
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>asdfsdf</div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
